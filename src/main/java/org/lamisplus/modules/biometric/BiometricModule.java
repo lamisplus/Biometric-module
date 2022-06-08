e60f2e7a42d3b7c41eb6a3f16586abb722668b41
Emeka <ilozuechukwuemeka@yahoo.com>
2022-06-08 15:58:28 +0100

    loading biometrics dependencies

--- src/main/java/org/lamisplus/modules/biometric/BiometricModule.java
+++ src/main/java/org/lamisplus/modules/biometric/BiometricModule.java
@@ -4,6 +4,15 @@ import com.foreach.across.core.AcrossModule;
 import com.foreach.across.core.annotations.AcrossDepends;
 import com.foreach.across.core.context.configurer.ComponentScanConfigurer;
 import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 
 @AcrossDepends(
 		required = {
@@ -11,6 +20,7 @@ import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
 		})
 public class BiometricModule extends AcrossModule {
 	public static final String NAME = "BiometricModule";
+	public static String modulePath = System.getProperty("user.dir");
 
 
 	public BiometricModule() {
@@ -29,5 +39,42 @@ public class BiometricModule extends AcrossModule {
 	@Override
 	public String getName() {
 		return  NAME;
+	}
+
+	@Bean
+	public void addDependencies(){
+		URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+		Class clazz= URLClassLoader.class;
+
+		// Use reflection
+		Method method= null;
+		try {
+			method = clazz.getDeclaredMethod("addURL", new Class[] { URL.class });
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+		method.setAccessible(true);
+
+
+		//String jarPath = modulePath+ File.separator+"java"+File.separator+"jar"+File.separator;
+		ArrayList<String> jarFileList = new ArrayList<String>();
+		jarFileList.add(modulePath+"FDxSDKPro-1.0.jar");
+		for(String jar : jarFileList){
+			File f = new File(jar);
+			if(f.exists() == false){
+				try {
+					throw new Exception("File [" + jar + "] doesn't exist!");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println("Adding jar [" + jar + "]");
+			try {
+				method.invoke(classLoader, new Object[] { f.toURL() });
+			} catch (IllegalAccessException | InvocationTargetException | MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
