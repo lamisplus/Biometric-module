a7a412aa17e56286d361a7e64372b786af8de97b
gichangi <gichangijohn3@gmail.com>
2022-07-18 09:11:43 +0300

    initiate
    creation of branch

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,7 @@
 
-//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-//export const url = '/api/v1/'
+/*export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'*/
 export const url =  'http://localhost:8282/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjU1NDEzOTk5fQ.Kc8WwA8t8O7M1mXTJMbNDBp9tvxaLd7-W2evaCBxiUpHT_pfIFLvSvQTq7eXyXJNQqCnSiLD1sXDKO0dqxAF0w';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjU4MTY3NDAzfQ.IEYgrfMmjvHq6VJ9beIemqSLZlQn8s8PLmowg0tw0wBae6fHjnY6xAFAPpr80-7ExJPP6S9cw3mSjuWRyo-m3Q';
+
+
